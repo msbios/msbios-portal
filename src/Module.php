0db2b7a8c60b6c96f9daaeda823f359391493504
Judzhin Miles <info@msbios.com>
2017-08-10 17:42:30 +0300

    Lazy Composer Commiter

--- src/Module.php
+++ src/Module.php
@@ -6,12 +6,15 @@
 namespace MSBios\Portal;
 
 use MSBios\ModuleInterface;
+use Zend\Loader\AutoloaderFactory;
+use Zend\Loader\StandardAutoloader;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 
 /**
  * Class Module
  * @package MSBios\Portal
  */
-class Module implements ModuleInterface
+class Module implements ModuleInterface, AutoloaderProviderInterface
 {
     /**
      * Returns configuration to merge with application configuration
@@ -21,5 +24,21 @@ class Module implements ModuleInterface
     public function getConfig()
     {
         return __DIR__ . '/../config/module.config.php';
+    }
+
+    /**
+     * Return an array for passing to Zend\Loader\AutoloaderFactory.
+     *
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return [
+            AutoloaderFactory::STANDARD_AUTOLOADER => [
+                StandardAutoloader::LOAD_NS => [
+                    __NAMESPACE__ => __DIR__,
+                ],
+            ],
+        ];
     }
 }
