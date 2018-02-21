f3fd25856b99ad5011a8e1bedd2b6eacad11d2d2
Judzhin Miles <info@msbios.com>
2018-02-21 14:17:18 +0200

    Lazy Composer Commiter

--- src/Module.php
+++ src/Module.php
@@ -20,7 +20,7 @@ class Module implements
 {
 
     /** @const VERSION */
-    const VERSION = '1.0.2';
+    const VERSION = '1.0.3';
 
     /**
      * Returns configuration to merge with application configuration
